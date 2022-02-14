b69cb2c7c384fa949bbd93182cd5dbf42b961974
w0xter <lpozogilo@gmail.com>
2022-02-14 13:43:04 +0100

    Minor change

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -1,18 +1,19 @@
 import React,{useState} from 'react'
 
-// var EmbedVideo = function(props) {
-//     return (
-//         <div dangerouslySetInnerHTML={{ __html: `
-//          <video
-//            muted
-//            autoplay
-//            playsinline
-//            src="${props.src}"
-//            class="${props.className}"
-//          />,
-//        ` }}></div>
-//     )
-//  }
+var EmbedVideo = function(props) {
+    return (
+        <div dangerouslySetInnerHTML={{ __html: `
+         <video
+            id="intro"
+            muted
+            autoplay
+            playsinline
+            src="${props.src}"
+            class="${props.className}"
+         />,
+       ` }}></div>
+    )
+ }
 
 export default function Intro(props){
     const [hide,setHide] = useState(false)
@@ -20,7 +21,7 @@ export default function Intro(props){
             hide?(
                 <></>
             ):(
-                <div className="fullScreen d-flex justify-content-center align-items-center">
+            <div className="fullScreen d-flex justify-content-center align-items-center">
                 <div className="container">
                     <video  id="intro" src="/assets/intro.mp4"   muted autoPlay onEnded={() => setHide(true)} className="img-fluid"></video>
                 </div>
